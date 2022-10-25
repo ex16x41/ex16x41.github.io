f25e2f623f58fcb51d01bf56d6d7ecdb693c4d8c
Eva Prokofiev <44981946+ex16x41@users.noreply.github.com>
2022-10-25 19:16:34 +0100

    Update script.js

--- tyr3/script.js
+++ tyr3/script.js
@@ -73,7 +73,7 @@ function search() {
 }
 function searchQuery(array) {
     array.forEach(extension => {
-        isBlocked(window.open(`http://google.com/search?q=site%3A${site.value}+filetype%3A${extension}%22`, "_blank"));
+        isBlocked(window.open(`http://google.com/search?q=site%3A${site.value}+filetype%3A${extension}`, "_blank"));
     });
 }
 function isBlocked(popupWindow){
