94813bf3f5c1ac657f0f63a48747cedd9941ceb1
Eva Prokofiev <44981946+ex16x41@users.noreply.github.com>
2022-10-25 19:34:02 +0100

    Create script2.js

---
+++ tyr3/script2.js
@@ -0,0 +1,77 @@
+var config = {
+    // Easily just add extensions bellow and the program will go through them
+    "Documents": [
+        "pdf",
+        "doc",
+        "docx",
+        "csv",
+        "xls",
+        "xlsx",
+        "txt",
+        "rtf",
+        "odt",
+        "ppt",
+        "pptx",
+        "pptm",
+        "xml",
+        "klm"
+    ],
+    "Databases": [
+        "php",
+        "sql",
+        "sqlite",
+        "pdb",
+        "idb",
+        "cdb",
+        "sis",
+        "odb"
+    ],
+    "Software": [
+        "-www"
+    ]
+};
+//--------------------------------------------------------------
+//get checkbox elements
+var checkDocuments = document.getElementById("searchDocuments");
+var checkDatabases = document.getElementById("searchDatabases");
+var checkSoftware = document.getElementById("searchSoftware");
+
+var counter = 0; //global counter for blocked popups
+
+//get text placeholders
+var site = document.getElementById('site');
+
+//--------------------------------------------------------------
+function search() {
+    counter = 0; //reset counter on every click 
+    console.log(counter);
+    //Checking if none of the checkboxes are checked and alerts the user
+    if (!checkDocuments.checked && !checkDatabases.checked && !checkSoftware.checked) {
+        alert("You have to check one option");
+    }
+
+    //Depending on the checkbox checked, run that query with array provided
+    if (checkDocuments.checked) {
+        searchQuery(config.Documents);
+    }
+    if (checkDatabases.checked) {
+        searchQuery(config.Databases);
+    }
+    if (checkSoftware.checked) {
+        searchQuery(config.Software);
+    }
+    // Check if it detected blocked popups
+    if (counter > 0) {
+        alert(`Our checker says ${counter} popups might have been blocked, please allow popups and try again!`);
+    }
+}
+function searchQuery(array) {
+    array.forEach(extension => {
+        isBlocked(window.open(`http://google.com/search?q=site%3A${site.value}+${extension}+%22`, "_blank"));
+    });
+}
+function isBlocked(popupWindow){
+    if (!popupWindow || popupWindow.closed || typeof popupWindow.closed == 'undefined') {
+        counter += 1;
+    }
+}
